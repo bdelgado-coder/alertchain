e3a07eecdd1cd298c66d055cf3d9fdf9a3fde133
Brandon Delgado <brandondelgado@MacBook-Pro.local>
2020-05-15 11:17:06 -0700

    Updated header file

--- include/alertchain.hpp
+++ include/alertchain.hpp
@@ -1,4 +1,5 @@
 #include <eosio/eosio.hpp>
+#include <eosio/system.hpp>
 
 using namespace std;
 using namespace eosio;
@@ -7,18 +8,23 @@ CONTRACT alertchain_book : public contract {
   public:
     using contract::contract;
 
-    ACTION regusr(name username);
-    ACTION unregusr(name username);
+    ACTION upsert(name user, std::string first_name, std::string last_name, std::string county);
+    ACTION erase(name user);
 
   private:
 
-    mailinglist4(name receiver, name code, datastream<const char*> ds);
+    alertchain_book(name receiver, name code, datastream<const char*> ds);
       contract(receiver, code, ds),
       mailinglist_table(receiver, receiver.value) {}
 
-    TABLE mailinglist {
-      name username;
-      auto primary_key() const { username.value }
+    TABLE person {
+      name key;
+			std::string first_name;
+			std::string last_name;
+			std::string county;
+      std::int nominations;
+
+			uint64_t primary_key() const { return key.value;}
   }
     typedef eosio::multi_index<name("mailinglist"), mailinglist> mailinglist_table;
 
